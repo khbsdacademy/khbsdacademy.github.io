091a331b67b5f48b723289313a7c177c9b644e47
FrostyBoys <bsd-12@BSD-12s-iMac.local>
2016-03-31 10:45:18 +0800

    test

--- index.php
+++ index.php
@@ -134,12 +134,7 @@
 						<div class="inner">
 							<h2 class="major">Get in touch</h2>
 							<p>Cras mattis ante fermentum, malesuada neque vitae, eleifend erat. Phasellus non pulvinar erat. Fusce tincidunt, nisl eget mattis egestas, purus ipsum consequat orci, sit amet lobortis lorem lacus in tellus. Sed ac elementum arcu. Quisque placerat auctor laoreet.</p>
-							<?php 
-$action=$_REQUEST['action']; 
-if ($action=="")    /* display the contact form */ 
-    { 
-    ?> 
-							<form method="post" action="">
+							<form method="post" action="emailHandle.php">
 								<div class="field">
 									<label for="name">Name</label>
 									<input type="text" name="name" id="name" />
